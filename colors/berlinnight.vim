ea6e2593a3085fb73a2d9ba7499c461c29ac3d98
waikoo <beroer@protonmail.com>
2024-10-01 02:04:59 +0200

    Fix: errors

--- colors/berlinnight.vim
+++ colors/berlinnight.vim
@@ -1,4 +1,10 @@
-lua << EOF
-local berlinnight = require("berlinnight")
-berlinnight.setup({})
-EOF
+" Set up the colorscheme
+set background=dark
+highlight clear
+if exists("syntax_on")
+  syntax reset
+endif
+let g:colors_name="berlinnight"
+
+" Load the Lua module
+lua require('berlinnight').load()
--- lua/berlinnight/init.lua
+++ lua/berlinnight/init.lua
@@ -1,18 +1,23 @@
 local M = {}
 local theme = require('berlinnight.theme')
 
-M.setup = function()
-  vim.cmd('hi clear')
+M.setup = function(opts)
+    -- You can add any setup options here if needed
+    -- For example: M.config = vim.tbl_deep_extend("force", {}, opts or {})
+end
 
-  vim.o.background = 'dark'
-  if vim.fn.exists('syntax_on') then
-    vim.cmd('syntax reset')
-  end
+M.load = function()
+    vim.cmd('hi clear')
 
-  vim.o.termguicolors = true
-  vim.g.colors_name = 'berlinnight'
+    vim.o.background = 'dark'
+    if vim.fn.exists('syntax_on') then
+        vim.cmd('syntax reset')
+    end
 
-  theme.set_highlights()
+    vim.o.termguicolors = true
+    vim.g.colors_name = 'berlinnight'
+
+    theme.set_highlights()
 end
 
 return M
