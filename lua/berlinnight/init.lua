ce9fe004b293f7f33a8e0175fb7c9150c7b7bd68
waikoo <beroer@protonmail.com>
2024-10-01 02:24:32 +0200

    Remove: debug statements, fix theme colors

--- lua/berlinnight/init.lua
+++ lua/berlinnight/init.lua
@@ -1,14 +1,12 @@
 local M = {}
 
 M.setup = function(opts)
-  print("Berlinnight setup called")
   -- You can add any setup options here if needed
 end
 
 M.load = function()
-  print("Berlinnight load called")
   local theme = require('berlinnight.theme')
-  
+
   vim.cmd('hi clear')
   vim.o.background = 'dark'
   if vim.fn.exists('syntax_on') then
